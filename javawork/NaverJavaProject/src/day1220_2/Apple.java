a4e2519e9bd425d1d2967c9b415a7340ed54b48b
seoyihyun <arrr1022@naver.com>
2024-12-20 16:03:42 +0900

    1220

---
+++ javawork/NaverJavaProject/src/day1220_2/Apple.java
@@ -0,0 +1,20 @@
+package day1220_2;
+
+public class Apple {
+	public void job() {
+		System.out.println("아침식사를 준비합니다");
+	}
+
+	protected void study() {
+		System.out.println("자바 공부를 합니다");
+	}
+	
+	void draw() {
+		System.out.println("그림 공부를 합니다");
+	}
+	
+	protected void process() {
+		System.out.println("DB 공부를 합니다");
+	}
+}
+
